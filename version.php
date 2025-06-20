5146eae62d5cec259cb6ea8d434eda69bc1c7f2a
Andrew Hancox <andrewdchancox@googlemail.com>
2025-06-20 16:06:16 +0100

    Bumpe version

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_topicsactivitycards';
 $plugin->release = '1.1.0';
-$plugin->version = 2024120613;
-$plugin->requires = 2023100900;
+$plugin->version = 2025062000;
+$plugin->requires = 2024100701;
 $plugin->maturity = MATURITY_STABLE;
