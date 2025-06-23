48428d24f8c407ff04a2edfe6dc720b12083e21d
Andrew Hancox <andrewdchancox@googlemail.com>
2025-06-23 16:53:20 +0100

    Fix bug when showcompletion enabled on course no completion

--- lib.php
+++ lib.php
@@ -74,7 +74,7 @@ class format_topicsactivitycards extends format_topics {
                 $PAGE->add_header_action
                 ($OUTPUT->render_from_template('format_topicsactivitycards/progress_doughnut', [
                     'showstatuspercentage' => true,
-                    'statuspercentage' => number_format(\core_completion\progress::get_course_progress_percentage($course)),
+                    'statuspercentage' => number_format(\core_completion\progress::get_course_progress_percentage($course) ?? 0),
                 ])
                 );
             }
